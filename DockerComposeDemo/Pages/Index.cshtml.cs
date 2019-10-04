c5c4f8b2ce13f98cd4e1b63203575b11c40b6f5c
冯辉 <695979933@qq.com>
2019-10-04 18:42:34 +0800

    模块 DockerComposeDemo 的初始签入

---
+++ DockerComposeDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceStack.Redis;
+
+namespace DockerComposeDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly RedisManagerPool redisManger = new RedisManagerPool("172.17.0.1:6379");
+        public void OnGet()
+        {
+            using (var db = redisManger.GetClient()) {
+                ViewData["num"] = db.IncrementValue("count");
+            } 
+        }
+    }
+}
